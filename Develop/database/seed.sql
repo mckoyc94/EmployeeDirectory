a0baeddd969063cb5124e02e00a8866517645744
Cmac <mckoyc@gmail.com>
2020-12-03 00:10:52 -0500

    updated code to include all departments

--- Develop/database/seed.sql
+++ Develop/database/seed.sql
@@ -1,11 +1,12 @@
+-- Departments
+INSERT INTO department (name) VALUES ('Management');
+INSERT INTO department (name) VALUES ('Desk');
+INSERT INTO department (name) VALUES ('Junior_Programs');
+INSERT INTO department (name) VALUES ('Belayers');
+INSERT INTO department (name) VALUES ('Setters');
+
+-- Roles
+INSERT INTO role (title, salary, department_id) VALUES (Gym Director, 40000, 1)
+
+-- Employees
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (Jordan, Pacheco, 1, 0);
-
-INSERT INTO department (name) VALUES ( Management);
-INSERT INTO department (name) VALUES ( Desk);
-INSERT INTO department (name) VALUES ( Junior Programs);
-INSERT INTO department (name) VALUES ( Belayers);
-INSERT INTO department (name) VALUES (Setters);
-
-INSERT INTO role (id, title, salary, department_id) VALUES (1, Gym Director,)
-
-
