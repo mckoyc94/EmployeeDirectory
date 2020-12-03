8aa3d40ca4d757269493571a9ef2a45b82e75d70
Cmac <mckoyc@gmail.com>
2020-12-03 00:33:41 -0500

    added roles to seed file

--- Develop/database/seed.sql
+++ Develop/database/seed.sql
@@ -6,7 +6,23 @@ INSERT INTO department (name) VALUES ('Belayers');
 INSERT INTO department (name) VALUES ('Setters');
 
 -- Roles
-INSERT INTO role (title, salary, department_id) VALUES (Gym Director, 40000, 1)
+INSERT INTO role (title, salary, department_id) VALUES ('Gym Director', 50000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Assistant Gym Director', 45000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Director of Maintenance', 40000, 1);
+
+INSERT INTO role (title, salary, department_id) VALUES ('Shift Supervisor', 25000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Front Desk', 20000, 2);
+
+INSERT INTO role (title, salary, department_id) VALUES ('Head Instructor', 15000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('Instructor', 10000, 3);
+
+INSERT INTO role (title, salary, department_id) VALUES ('Facilitator', 15000, 4);
+INSERT INTO role (title, salary, department_id) VALUES ('Belayer', 8000, 4);
+
+INSERT INTO role (title, salary, department_id) VALUES ('Setter', 13000, 5);
+INSERT INTO role (title, salary, department_id) VALUES ('Maintanence', 12000, 5);
+
 
 -- Employees
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (Jordan, Pacheco, 1, 0);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Jordan', 'Pacheco', 1, 0);
+
